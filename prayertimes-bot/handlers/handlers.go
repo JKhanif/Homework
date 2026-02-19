97f8668801d10338ba6fb97d7479753f011ba633
JKhanif <jamal005@list.ru>
2026-02-19 14:33:56 +0300

    Добавил пакет service

--- prayertimes-bot/handlers/handlers.go
+++ prayertimes-bot/handlers/handlers.go
@@ -2,24 +2,20 @@ package handlers
 
 import (
 	"context"
-	"fmt"
 	"log"
-	"prayertimes/clients/aladhan"
-	"time"
+	"prayertimes/service"
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
 )
 
-var prayerNames = [5]string{"Фаджр", "Зухр", "Аср", "Магриб", "Иша"}
-
 type Handler struct {
-	aladhanClient *aladhan.Client
+	service *service.Service
 }
 
-func New(aladhanClient *aladhan.Client) *Handler {
+func New(service *service.Service) *Handler {
 	return &Handler{
-		aladhanClient: aladhanClient,
+		service: service,
 	}
 }
 
@@ -29,10 +25,9 @@ func (h *Handler) PrayerNow(ctx context.Context, b *bot.Bot, update *models.Upda
 
 func (h *Handler) Handle(ctx context.Context, b *bot.Bot, update *models.Update) {
 	city := update.Message.Text
-
-	res, err := h.aladhanClient.GetTodayPrayerTimesByCity(ctx, city)
+	res, err := h.service.ResponsePrayerTime(ctx, city)
 	if err != nil {
-		log.Println("error h.GetTodayPrayerTimesByCity: ", err)
+		log.Println("error h.service.ResponsePrayerTime: ", err)
 		b.SendMessage(ctx, &bot.SendMessageParams{
 			ChatID: update.Message.Chat.ID,
 			Text:   "Ошибка. Попробуйте позже.",
@@ -40,42 +35,9 @@ func (h *Handler) Handle(ctx context.Context, b *bot.Bot, update *models.Update)
 		return
 	}
 
-	timeStr := []string{res.Data.Timings.Fajr, res.Data.Timings.Dhuhr, res.Data.Timings.Asr, res.Data.Timings.Maghrib, res.Data.Timings.Isha}
-
-	nextPrayerFound := false
-	resMessage := ""
-
-	for i, v := range timeStr {
-		now := time.Now()
-		parsed, err := time.Parse("15:04", v)
-		if err != nil {
-			log.Println("error time.Parse: ", err)
-			b.SendMessage(ctx, &bot.SendMessageParams{
-				ChatID: update.Message.Chat.ID,
-				Text:   "Ошибка. Попробуйте позже.",
-			})
-			return
-		}
-
-		prayerTime := time.Date(now.Year(), now.Month(), now.Day(), parsed.Hour(), parsed.Minute(), 0, 0, now.Location())
-
-		if nextPrayerFound == false && now.Before(prayerTime) {
-			nextPrayerFound = true
-			resMessage += fmt.Sprintf("<b>%s: %s</b>\n", prayerNames[i], v)
-			continue
-		}
-
-		resMessage += fmt.Sprintf("%s: %s\n", prayerNames[i], v)
-	}
-
-	now := time.Now().Format("02.01.2006")
-
-	hijriTime := fmt.Sprintf("\n%s %s %s", res.Data.Date.Hijri.Day, res.Data.Date.Hijri.Month.En, res.Data.Date.Hijri.Year)
-	resMessage = fmt.Sprintf("Город: %s\nРасписание на %s\n\n%s%s", city, now, resMessage, hijriTime)
-
 	b.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID:    update.Message.Chat.ID,
-		Text:      resMessage,
+		Text:      res,
 		ParseMode: models.ParseModeHTML,
 	})
 }
---
+++ prayertimes-bot/service/service.go
@@ -0,0 +1,65 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"prayertimes/clients/aladhan"
+	"time"
+)
+
+type Service struct {
+	aladhanClient *aladhan.Client
+}
+
+func New(aladhanClient *aladhan.Client) *Service {
+	return &Service{
+		aladhanClient: aladhanClient,
+	}
+}
+
+var prayerNames = [5]string{"Фаджр", "Зухр", "Аср", "Магриб", "Иша"}
+
+func (s *Service) ResponsePrayerTime(ctx context.Context, city string) (string, error) {
+	res, err := s.aladhanClient.GetTodayPrayerTimesByCity(ctx, city)
+	if err != nil {
+		log.Println("error s.aladhanClient.GetTodayPrayerTimesByCity: ", err)
+		return "", err
+	}
+
+	timeStr := []string{res.Data.Timings.Fajr, res.Data.Timings.Dhuhr, res.Data.Timings.Asr, res.Data.Timings.Maghrib, res.Data.Timings.Isha}
+
+	nextPrayerFound := false
+	resMessage := ""
+	now := time.Now()
+
+	for i, v := range timeStr {
+		parsed, err := time.Parse("15:04", v)
+		if err != nil {
+			log.Println("error time.Parse: ", err)
+			return "", err
+		}
+
+		prayerTime := time.Date(now.Year(), now.Month(), now.Day(), parsed.Hour(), parsed.Minute(), 0, 0, now.Location())
+
+		if nextPrayerFound == false && now.Before(prayerTime) {
+			nextPrayerFound = true
+			nextTP := prayerTime.Sub(now)
+			h := int(nextTP.Hours())
+			m := int(nextTP.Minutes()) % 60
+			timeLeft := fmt.Sprintf("%02d:%02d", h, m)
+			resMessage += fmt.Sprintf("<b>%s: %s</b>", prayerNames[i], v)
+			resMessage += fmt.Sprintf("(Через %s)\n", timeLeft)
+			continue
+		}
+
+		resMessage += fmt.Sprintf("%s: %s\n", prayerNames[i], v)
+	}
+
+	format := time.Now().Format("02.01.2006")
+
+	hijriTime := fmt.Sprintf("\n%s %s %s", res.Data.Date.Hijri.Day, res.Data.Date.Hijri.Month.En, res.Data.Date.Hijri.Year)
+	resMessage = fmt.Sprintf("🕌Город: %s\nРасписание на %s\n\n%s%s", city, format, resMessage, hijriTime)
+
+	return resMessage, nil
+}
